e384e0535df560caf4bc0dd3b0491ca809adbcfc
minyu zhang <minyu@Alienware>
2024-08-01 12:31:36 -0500

    Add User login backend endpoint

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,20 +1,10 @@
 // backend/routes/api/index.js
 const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
+// GET /api/restore-user
+const { restoreUser } = require('../../utils/auth.js');
 
-// router.post('/test', function (req, res) {
-//     // test in the browser's devtools console.phase-1
-//     // fetch('/api/test', {
-//     //     method: "POST",
-//     //     headers: {
-//     //         "Content-Type": "application/json",
-//     //         "XSRF-TOKEN": "sPVbz7xd-5DMsDGhmFN0HMg8q0j1GyUnbzrk"
-//     //     },
-//     //     body: JSON.stringify({ hello: 'world' })
-//     // }).then(res => res.json()).then(data => console.log(data));
-
-//     res.json({ requestBody: req.body });
-
-// });
 
 // GET /api/set-token-cookie
 const { setTokenCookie } = require('../../utils/auth.js');
@@ -30,10 +20,28 @@ const { User } = require('../../db/models');
 // });
 
 
-// GET /api/restore-user
-const { restoreUser } = require('../../utils/auth.js');
+
 
 router.use(restoreUser);
+
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
+
+router.post('/test', function (req, res) {
+    // test in the browser's devtools console.phase-1
+    // fetch('/api/test', {
+    //     method: "POST",
+    //     headers: {
+    //         "Content-Type": "application/json",
+    //         "XSRF-TOKEN": "sPVbz7xd-5DMsDGhmFN0HMg8q0j1GyUnbzrk"
+    //     },
+    //     body: JSON.stringify({ hello: 'world' })
+    // }).then(res => res.json()).then(data => console.log(data));
+
+    res.json({ requestBody: req.body });
+
+});
 
 // router.get(
 //   '/restore-user',
