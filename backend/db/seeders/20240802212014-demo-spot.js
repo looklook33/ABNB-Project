e8f53fd8ebbef699d67d9c7310a33adcfd2db276
minyu zhang <minyu@Alienware>
2024-08-31 14:11:10 -0500

    fix character limit

--- backend/db/seeders/20240802212014-demo-spot.js
+++ backend/db/seeders/20240802212014-demo-spot.js
@@ -18,7 +18,7 @@ const spots = [
     lat: 37.7645358,
     lng: -122.4730327,
     name: "Hyatt",
-    description: "This hotel is pretty much to the best hotel I have stayed in.The service from the dinning staff and room service was excellent. The front desk checking in process could use more training and accommodate mistake during booking of hotel. The food that the hotel has to offer was a great variety and the taste was great. The beaches were beautiful. My family had a great time here and will be coming back sometime in the near future. What a great place to relax and enjoy.",
+    description: "This hotel is pretty much to the best hotel I have stayed in.",
     price: 140,
   },
   {
@@ -30,7 +30,7 @@ const spots = [
     lat: 40.7645358,
     lng: -120.4730327,
     name: "Luxury Knolls Mansion",
-    description:  "What's not to love about this place! One of the most unique homes on the lake comes with the best views on the lake, private outdoor pool and hot tub, excellent location and gives you access to all of the fantastic Knolls amenities (shared indoor pool/hot tub, sauna, steam room, tennis courts and more). Not that you need to ever leave the house, but if you do you're a short drive or boat ride to Redhead Yacht Club, Margaritaville Landshark Bar, Shorty Pants and many other great spots!",
+    description:  "What's not to love about this place!",
     price: 140,
   },
   {
@@ -42,7 +42,7 @@ const spots = [
     lat: 50.7645358,
     lng: -100.4730327,
     name: "The Lighthouse",
-    description: "The Lighthouse is a lakefront property close to downtown Wisconsin Dells. You’ll love our house because of the coziness, the location, and the views. Also the complimentary use of three kayaks, two row boats and one canoe!  Our house is good for couples, families (with kids), and groups up to 14.",
+    description: "The Lighthouse is a lakefront property close to downtown Wisconsin Dells.",
     price: 140,
   },
   {
@@ -54,7 +54,7 @@ const spots = [
     lat: 33.7645358,
     lng: -34.4730327,
     name: "Kauai Island",
-    description:  "Cute little plantation style facility. Older but clean and off the beaten path which we loved. Breakfast would be classified as more of a continental breakfast of which choices were not as healthy as a diabetic would hope for So we did opt to eat breakfast away from there. Staff was very friendly and helpful. Pool was small but well kept. Would stay again.",
+    description:  "Cute little plantation style facility.",
     price: 200,
   },
   {
@@ -65,8 +65,8 @@ const spots = [
     country: "United States of America",
     lat: 21.7645358,
     lng: -39.4730327,
-    name: "Exquisite Private Lake Front Family Hom",
-    description: "3.5 acre end of the road Custom built home, on a small lake perfect for kayaking or fishing. Within minutes to multiple larger lakes and beaches. This area is secluded, spacious and peaceful  with many different areas on the property to relax with friends and family. The large deck is a great place to grill out and to watch the spectacular sunsets. Perfect for a week long stay.  Five quaint river towns are within a half hour drive.",
+    name: "APP island",
+    description: "3.5 acre end of the road Custom built home, on a small lake perfect for kayaking or fishing. Within minutes to multiple larger lakes and beaches. This area is secluded, spacious and peaceful.",
     price: 210,
   },
   {
@@ -78,7 +78,7 @@ const spots = [
     lat: 63.7645358,
     lng: -80.4730327,
     name: "Sunrise Sanctuary",
-    description:"SUNRISE SANCTUARY has a Native American theme with vivid colors and designs that follow somewhat of a Southwestern flair, but holding true to the Native American love and respect for our wildlife, beautiful mountainous land and clean unspoiled environment, free from the hustles and bustles of city life.",
+    description:"SUNRISE SANCTUARY has a Native American theme with vivid colors and designs that follow somewhat of a Southwestern flair, but holding true to the Native American love and respect for our wildlife.",
     price: 230,
   },
   {
@@ -89,8 +89,8 @@ const spots = [
     country: "United States of America",
     lat: 35.7645358,
     lng: -36.4730327,
-    name:  "Cove at 420: Modern Lake Front Home near Chicago",
-    description:  "Welcome to the Cove at 420.  A modern vacation paradise where indoor and outdoor lines blur.  Every space is designed for enjoyment.  A short 75 minute drive from Chicago, this is a true retreat.  Wake up to beautiful sunrise views or enjoy them from a kayak on the water.  We offer kayaks, paddle boards, a catamaran, hot tub & sauna, Sonos sound system, a fire pit and plenty of yard games for our guests.",
+    name:  "Modern Lake",
+    description:  "Welcome to the Cove at 420. A modern vacation paradise where indoor and outdoor lines blur.  Every space is designed for enjoyment.  A short 75 minute drive from Chicago, this is a true retreat.  Wake up to beautiful sunrise.",
     price: 123,
   },
   {
